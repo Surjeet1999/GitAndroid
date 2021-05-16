4bc9abce705e7bc7bb60ac44a5466407c97d10c4
Surjeet1999 <surjeetrawat246@gmail.com>
2021-05-16 20:37:51 +0530

    hello world log added again

--- app/src/main/java/com/example/gitandroid/MainActivity.java
+++ app/src/main/java/com/example/gitandroid/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("TAG", "onCreate: "+"message");
         Log.d("TAG", "onCreate: "+ "Hello World!");
+        Log.d("TAG", "onCreate: "+ "Hello World!");
     }
 }
